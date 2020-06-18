d67b1d7ae47ab32f0c26d5d341144a4bb1232261
JosueBarto97 <37382075+JosueBarto97@users.noreply.github.com>
2020-06-18 14:54:32 -0400

    Delete ejercicio4.py

--- ejercicio4.py
+++
@@ -1,9 +0,0 @@
-a = input("ingrese edad 1:   ")
-b = input("ingrese edad 2:   ")
-
-if a<b:
-	print ("la persona 1 es menor que la persona 2")
-elif a==b:
-	print ("ambos tiene la misma edad")
-else:
-	print ("la persona 1 es mayor que la persona 2")
