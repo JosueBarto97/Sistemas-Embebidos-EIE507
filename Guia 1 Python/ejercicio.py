adcd9028b2801c1487d99d1fd76e055138dd173b
JosueBarto97 <37382075+JosueBarto97@users.noreply.github.com>
2020-06-18 14:57:45 -0400

    Add files via upload

---
+++ Guia 1 Python/ejercicio.py
@@ -0,0 +1,10 @@
+print ("Area de un circulo \n ")
+
+pi=3.1415926535
+
+Radio=int(input("Ingrese el valor del radio:  "))
+
+area=(Radio**2)*pi
+
+print ("el area es :  " ,area)
+
---
+++ Guia 1 Python/ejercicio2.py
@@ -0,0 +1,11 @@
+a='b'
+
+print (a)
+
+print (a+'b')
+
+print (a+'a')
+
+print (a*2+'b'*3)
+
+print (2*(a+'b'))
